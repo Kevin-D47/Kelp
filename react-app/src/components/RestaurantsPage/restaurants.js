ad02f78dd89c52b95a0f94798ac9a73ded04e3c8
Kevin Duong <kevduong47@gmail.com>
2022-10-25 19:12:48 -0400

    commented out docker file for now

--- react-app/src/components/RestaurantsPage/restaurants.js
+++ react-app/src/components/RestaurantsPage/restaurants.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllBusinessesThunk } from "../../store/businesses";
 import { NavLink } from 'react-router-dom'
+import './restaurants.css'
 
 
 const GetAllBusinesses = () => {

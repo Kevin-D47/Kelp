03ef0e134dbfef7b66a67edc2b1054c2de6b21ed
Kevin Duong <kevduong47@gmail.com>
2022-10-26 19:37:39 -0400

    stylized restraunts page

--- app/seeds/businesses.py
+++ app/seeds/businesses.py
@@ -1,9 +1,47 @@
 from app.models import Business, db
 
+
 def seed_businesses():
-    business_1 = Business(userId=1, address='1234 Addy', city='Atlanta', state='GA', country='USA', zip=30087, name='Krusty Krab', description='something', phone=6783490689, price='$', previewImageUrl='previewURL1')
-    business_2 = Business(userId=2, address='4567 Addy', city='New York City', state='NY', country='USA', zip=50023, name='Mussle City', description='something1', phone=9775271337, price='$$', previewImageUrl='previewURL2')
-    business_3 = Business(userId=3, address='7890 Addy', city='Los Angeles', state='CA', country='USA', zip=90025, name='Big Fish Dinner', description='something2', phone=2335041994, price='$$$$', previewImageUrl='previewURL3')
+    business_1 = Business(
+        userId=1,
+        address='1234 Addy',
+        city='Atlanta', state='GA',
+        country='USA',
+        zip=30087,
+        name='Krusty Krab',
+        description='Who lives in a pineapple under the sea? SpongeBob SquarePants! Absorbent and yellow and porous is he SpongeBob SquarePants!',
+        phone=6783490689,
+        price='$',
+        previewImageUrl='https://i.pinimg.com/originals/33/e9/31/33e9318abad9a87418c3eace3effae30.jpg'
+    )
+
+    business_2 = Business(
+        userId=2,
+        address='4567 Addy',
+        city='New York City',
+        state='NY',
+        country='USA',
+        zip=50023,
+        name='Mussle City',
+        description='Here you can find the biggest most shredded mussles. We work hard to get our mussles as pumped and juiciest around',
+        phone=9775271337,
+        price='$$',
+        previewImageUrl='https://i.ndtvimg.com/i/2015-10/mussels-625_625x350_61444640859.jpg'
+    )
+
+    business_3 = Business(
+        userId=3,
+        address='7890 Addy',
+        city='Los Angeles',
+        state='CA',
+        country='USA',
+        zip=90025,
+        name='Seaside Hooks',
+        description='We only serve the freshs catch of the day. Indulge on all the best dishes that out oceans offer here',
+        phone=2335041994,
+        price='$$$$',
+        previewImageUrl='https://s3-media0.fl.yelpcdn.com/bphoto/Zg95QGwmay_PpgV0Xecb6A/o.jpg'
+    )
 
     businesses = [business_1, business_2, business_3]
     for business in businesses:
--- react-app/src/components/RestaurantsPage/restaurants.js
+++ react-app/src/components/RestaurantsPage/restaurants.js
@@ -21,15 +21,35 @@ const GetAllBusinesses = () => {
     }
 
     return (
-        <div>
-             <h2>Restraunts Page</h2>
-            {allBusinessesArr.map((business) => {
-                return (
-                    <div key={business.id}>
-                        {business.name}
-                    </div>
-                )
-            })}
+        <div className="restraunts-container">
+            <div className="restraunts-wrapper">
+                <h2>The Best Seafood Restaurants to visit</h2>
+                <h3>Results</h3>
+                {allBusinessesArr.map((business) => {
+                    return (
+                        <div className="all-restraunts-container" key={business.id}>
+                            <NavLink className="single-rest-container" to={`/businesses/${business.id}`}>
+                                <div className="single-rest-container-left">
+                                    <img className="restraunt-img" src={business.previewImageUrl}></img>
+                                </div>
+                                <div className="single-rest-container-right">
+                                    <div className="restraunt-name">{business.name}</div>
+                                    <div className="restraunt-info-container">
+                                        {/* placeholder for ratings and #reviews */}
+                                        <div>Ratings</div>
+                                        <div> #Reviews</div>
+                                        <div>{business.price}</div>
+                                    </div>
+                                    <div className="restraunt-location">
+                                        {business.city}, {business.state}
+                                    </div>
+                                    <div className="restraunt-description">{business.description}</div>
+                                </div>
+                            </NavLink>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }

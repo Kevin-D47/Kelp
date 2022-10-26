c32d0f817b09ec580bf1a2d1bc707f2ab3ce79b6
Kevin Duong <kevduong47@gmail.com>
2022-10-26 13:39:06 -0400

    save prog

--- react-app/src/components/BusinessDetailsPage/businessDetails.js
+++ react-app/src/components/BusinessDetailsPage/businessDetails.js
@@ -4,15 +4,34 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams, NavLink, useHistory } from "react-router-dom";
 
 import { getAllBusinessesThunk } from '../../store/businesses';
+import { getOneBusinessThunk } from '../../store/businesses';
 import { getAllUsersThunk } from '../../store/users';
 
 import './businessDetails.css'
 
 
 const BusinessDetails = () => {
+    const dispatch = useDispatch();
+    const history = useHistory()
+
+    const { businessId } = useParams()
+
+    const sessionUser = useSelector(state => state.session.user)
+    const currBusiness = useSelector(state => state.businesses[businessId])
+
+
+    useEffect(() => {
+        dispatch(getOneBusinessThunk(businessId))
+    }, [dispatch, businessId])
+
     return (
-        <h2>Business Details Page</h2>
+        <div >
+            <h2> Business Details Page</h2 >
+            <h2>{currBusiness.name}</h2>
+        </div>
     )
+
+
 }
 
 export default BusinessDetails

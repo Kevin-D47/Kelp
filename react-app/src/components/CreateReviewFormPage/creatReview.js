8ff3838cd7521848ae1a5955091af7375ec22d69
Kevin Duong <kevduong47@gmail.com>
2022-10-29 23:32:21 -0400

    stylized create review form page

--- react-app/src/components/CreateReviewFormPage/creatReview.js
+++ react-app/src/components/CreateReviewFormPage/creatReview.js
@@ -4,6 +4,8 @@ import { Redirect, useHistory, useParams } from "react-router-dom";
 
 import { createReviewThunk } from '../../store/reviews'
 import { getOneBusinessThunk } from '../../store/businesses'
+import { getBusinessReviewsThunk, } from '../../store/reviews'
+import { getAllUsersThunk } from '../../store/users'
 
 import './createReview.css'
 
@@ -19,12 +21,24 @@ function CreateReviewForm() {
     const sessionUser = useSelector((state) => state.session.user);
     const userId = sessionUser.id
 
+    const allReviews = useSelector(state => state.reviews)
+    const getAllReviewArr = Object.values(allReviews)
+
+    const allUsers = useSelector(state => state.users)
+    const allUsersArr = Object.values(allUsers)
+
     const [isLoaded, setIsLoaded] = useState(false)
     const [review, setReview] = useState("");
     const [stars, setStars] = useState("");
+    const [currReview, setCurrReview] = useState(false)
 
     const [errors, setErrors] = useState([]);
     const [hasSubmitted, setHasSubmitted] = useState(false)
+
+
+    useEffect(() => {
+        dispatch(getAllUsersThunk())
+    }, [dispatch])
 
 
     useEffect(() => {
@@ -46,6 +60,15 @@ function CreateReviewForm() {
         dispatch(getOneBusinessThunk(businessId)).then(() => setIsLoaded(true))
     }, [dispatch, businessId])
 
+    useEffect(() => {
+        dispatch(getBusinessReviewsThunk(businessId)).then(() => setIsLoaded(true))
+    }, [dispatch, businessId])
+
+    if (!getAllReviewArr.length) {
+        return null
+    }
+
+
     const onSubmit = async (e) => {
         e.preventDefault();
 
@@ -63,52 +86,48 @@ function CreateReviewForm() {
     };
 
     const errorList = errors.map((error) => (
-        <p key={error}>{error}</p>
+        <p className='create-review-single-error' key={error}>{error}</p>
     ))
 
     return (
         isLoaded && (
             <div className="create-review-container">
+
                 <div className="create-review-container-left">
+                    <div className="create-review-header-container">
+                        <div style={{ fontSize: '30px' }}>Create a Review</div>
+                        <div>Create a review by filling out the inputs below.</div>
+                    </div>
+                    <div className="create-review-errors">
+                        {hasSubmitted && errorList}
+                    </div>
                     <form className="create-review-form" onSubmit={onSubmit}>
-                        <div className="create-review-header-container">
-                            <h3 className="create-review-header">Create a Review</h3>
+                        <div className="create-rating-container">
+                            <input
+                                className="rating-input"
+                                type="number"
+                                placeholder="1 - 5 stars"
+                                minLength="1"
+                                maxLength="5"
+                                step="1"
+                                value={stars}
+                                onChange={(e) => setStars(e.target.value)}
+                            />
+                            <div style={{ fontSize: '20px' }}>Select your rating</div>
                         </div>
-                        <div className="create-review-errors">
-                            {hasSubmitted && errorList}
+                        <div className="create-review-input-container">
+                            <div className='create-review-input-title'>Review:</div>
+                            <textarea
+                                className="create-review-input"
+                                type="string"
+                                placeholder="What was it like to stay here?"
+                                value={review}
+                                onChange={(e) => setReview(e.target.value)}
+                            />
                         </div>
-                        <div className="modal-body">
-                            <label className="create-review-label">
-                                Write a review here:
-                                <div className="create-review-input-container">
-                                    <textarea
-                                        className="review-input"
-                                        type="string"
-                                        placeholder="What was it like to stay here?"
-                                        value={review}
-                                        onChange={(e) => setReview(e.target.value)}
-                                    />
-                                </div>
-                            </label>
-                            <label className="create-review-label">
-                                Rating:
-                                <div className="rating-input-container">
-                                    <input
-                                        className="rating-input"
-                                        type="number"
-                                        placeholder="1 - 5 stars"
-                                        minLength="1"
-                                        maxLength="5"
-                                        step="1"
-                                        value={stars}
-                                        onChange={(e) => setStars(e.target.value)}
-                                    />
-                                </div>
-                            </label>
-                        </div>
-                        <div className="review-submit-container">
+                        <div className="create-review-submit-container">
                             <button
-                                className="create-review-button"
+                                className="create-review-form-button"
                                 type="submit"
                                 disabled={hasSubmitted && errors.length > 0}
                             >
@@ -116,6 +135,43 @@ function CreateReviewForm() {
                             </button>
                         </div>
                     </form>
+                </div>
+                <div className="create-review-container-right">
+                    <div style={{ fontSize: "24px", fontWeight: '400' }}>Recent Reviews</div>
+                    {getAllReviewArr.map(review => {
+                        return (
+                            <div className='create-single-review-container' key={review.id}>
+                                <div className='user-pic-name-options-conatiner' style={{ fontSize: '18px', fontWeight: 'bold' }}>
+                                    <div className='box'>
+                                        <div className='user-name'>
+                                            {allUsersArr && allUsersArr.map(user => {
+                                                return (
+                                                    <> {review.userId === user.id ? (
+                                                        <div className='user-pic-name' key={review.userId === user.id ? user.id : ''}>
+                                                            <img className='reviewUserPic' src={review.userId === user.id ? user.profileImageUrl : ''}></img>
+                                                            {review.userId === user.id ? user.first_name : ''}
+                                                            {review.userId === user.id ? user.last_name : ''}
+                                                        </div>) : ''}
+                                                    </>
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='review-body'>
+                                    <div className='review-rating-date'>
+                                        <div className='stars-placeholder'>
+                                            {review.stars} kelp
+                                        </div>
+                                        <div>{review.created_at.slice(8, 11)} {review.created_at.slice(5, 7)}, {review.created_at.slice(12, 16)}</div>
+                                    </div>
+                                    <div className='review'>
+                                        {review.review}
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         )

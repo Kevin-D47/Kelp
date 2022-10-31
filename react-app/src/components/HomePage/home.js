7533664bd7d5151b69f5ad0574945b3cd7d9a472
Kevin Duong <kevduong47@gmail.com>
2022-10-30 22:25:29 -0400

    stylized homepage WIP

--- react-app/src/components/HomePage/home.js
+++ react-app/src/components/HomePage/home.js
@@ -1,10 +1,82 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React from 'react'
+import { useEffect, useState } from "react";
+import { NavLink } from 'react-router-dom';
 import "./home.css";
 
+import githubIcon from '../../icons/github-icon.png'
+import linkedinIcon from '../../icons/linkedin-icon.png'
+import kelpLogo from '../../icons/Kelp-logo.png'
+import kelpTitleLogo from '../../icons/kelp-logo-title.png'
+
+
 const HomePage = () => {
+    const img1 = 'https://s3-media0.fl.yelpcdn.com/bphoto/Zg95QGwmay_PpgV0Xecb6A/o.jpg'
+    const img2 = 'https://s3-media0.fl.yelpcdn.com/bphoto/3PVczqE9frDnDmdcjXed-Q/o.jpg'
+    const img3 = 'https://i.ndtvimg.com/i/2015-10/mussels-625_625x350_61444640859.jpg'
+
+    const carouselArr = [img1, img2, img3]
+
+    const [image, setImage] = useState(carouselArr[0])
+    const [counter, setCounter] = useState(0)
+
+    useEffect(() => {
+        setImage(carouselArr[counter])
+    }, [counter])
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCounter((counter) => counter === 2 ? 0 : counter + 1)
+        }, 3000)
+        return () => clearInterval(timer)
+    }, [])
+
     return (
-        <h2>HomePage</h2>
+        <div className='homepage-container'>
+            <div className='homepage-images-container'>
+                <img className="homepage-image" src={image}></img>
+            </div>
+            <div className='homepage-title-container'>
+                <div className='homepage-title'>Get some Seafood today</div>
+                <NavLink to='/sign-up'>
+                <div className='homepage-signup-bttn'>Sign Up Here</div>
+              </NavLink>
+            </div>
+            <div className="hompage-footer-container">
+                <div className='hompage-footer-wrapper'>
+                    <div className="about-footer-container">
+                        <div style={{ fontSize: '20px', fontWeight: 'bold' }}>About</div>
+                        <a className="footer-link" href="https://github.com/Kevin-D47/Kelp" target="_blank">
+                            <img className='footer-icon' src={githubIcon}></img>
+                            <div className="kelp-github-repo">Kelp Github Repository</div>
+                        </a>
+                    </div>
+                    <div className="developer-footer-container">
+                        <div style={{ fontSize: '20px', fontWeight: 'bold' }}>Developer</div>
+                        <div style={{ fontWeight: '500' }}>Kevin Duong</div>
+                        <div className='kevin-links'>
+                            <a className="footer-link" href="https://www.linkedin.com/in/kevin-duong-513341216/" target="_blank">
+                                <img className='footer-icon' src={linkedinIcon}></img>
+                                <div className="kelp-github-repo">LinkedIn</div>
+                            </a>
+                            <a className="footer-link" href="https://github.com/Kevin-D47" target="_blank">
+                                <img className='footer-icon' src={githubIcon}></img>
+                                <div className="kelp-github-repo">Github</div>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='copyright-container'>
+                    <div>Copyright © 2022 Kelp Inc. Kelp,</div> &nbsp;
+                    <img className='coyright-icon' src={kelpTitleLogo}></img>
+                    <div>,</div> &nbsp;
+                    <img className='coyright-icon-2' src={kelpLogo}></img> &nbsp;
+                    <div>and related marks are registered trademarks of Kelp.</div>
+                </div>
+
+            </div>
+        </div>
+
     );
 
 }

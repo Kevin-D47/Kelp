5bc9bf541b4489f90f72436d4181c2b3b57a2514
Kevin Duong <kevduong47@gmail.com>
2023-01-11 15:09:51 -0500

    404 page WIP

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
@@ -74,12 +74,13 @@ function App() {
         <ProtectedRoute path='/users/:userId' exact={true} >
           <User />
         </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true}>
+        {/* <ProtectedRoute path='/' exact={true}>
           <h1>My Home Page</h1>
-        </ProtectedRoute>
+        </ProtectedRoute> */}
         <Route>
-          <PageNotFound />
+          <PageNotFound path='*' />
         </Route>
+        <Redirect to="/404" />
       </Switch>
     </BrowserRouter>
   );
